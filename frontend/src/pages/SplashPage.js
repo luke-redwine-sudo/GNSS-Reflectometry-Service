eecaa0b86cb1373284a5b80b00c450b7299f99d4
luke-redwine-sudo <Luke.A.Redwine@gmail.com>
2023-12-18 16:23:44 -0500

    Placed components into separate files

---
+++ frontend/src/pages/SplashPage.js
@@ -0,0 +1,18 @@
+
+function SplashPage() {
+    return (
+        <main>
+            <section id="hero">
+                <h1>GNSS Reflectometry Service</h1>
+                <p>Welcome to the GNSS Reflectometry Service</p>
+                <div class="home_buttons">
+                    <button>UPLOADS</button>
+                     <button>ANALYSIS</button>
+                </div>
+            </section>
+        </main>
+  );
+}
+
+
+export default SplashPage;
