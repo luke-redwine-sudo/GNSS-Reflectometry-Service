25b24026f1f438869fb14e155e21df8d74e12d00
luke-redwine-sudo <Luke.A.Redwine@gmail.com>
2023-12-18 10:06:22 -0500

    Stationary Layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,13 +11,6 @@ function App() {
                       <li><a href="uploads">UPLOADS</a></li>
                       <li><a href="analysis">ANALYSIS</a></li>
                   </ul>
-                  <div class="toggle_btn">
-                      <i class="fa-solid fa-bars"></i>
-                  </div>
-              </div>
-              <div class="dropdown_menu">
-                  <li><a href="uploads">Uploads</a></li>
-                  <li><a href="analysis">Analysis</a></li>
               </div>
           </header>
           <main>
@@ -30,21 +23,9 @@ function App() {
                   </div>
               </section>
           </main>
-          <script>
-              const toggleBtn = document.querySelector(".toggle_btn");
-              const toggleBtnIcon = document.querySelector(".toggle_btn i");
-              const dropDownMenu = document.querySelector(".dropdown_menu");
-              toggleBtn.onclick = toggleButtonFunction(dropDownMenu, toggleBtnIcon);
-
-          </script>
       </>
   );
 }
 
-function toggleButtonFunction(dropDownMenu, toggleBtnIcon) {
-    dropDownMenu.classList.toggle("open")
-    const isOpen = dropDownMenu.classList.contains("open")
-    toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars"
-}
 
 export default App;
--- frontend/src/toggleButtonFunction.js
+++
@@ -1,9 +0,0 @@
-
-
-function toggleButtonFunction(dropDownMenu, toggleBtnIcon) {
-    return (
-        dropDownMenu.classList.toggle("open");
-        const isOpen = dropDownMenu.classList.contains("open");
-        toggleBtnIcon.classList = isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars";
-    );
-}
