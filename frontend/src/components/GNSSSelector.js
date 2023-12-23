5eb435cf2b5978b553faf1886cdbd0c73a42a910
luke-redwine-sudo <Luke.A.Redwine@gmail.com>
2023-12-23 00:19:11 -0500

    GNSS Form is now working

--- frontend/src/components/GNSSSelector.js
+++ frontend/src/components/GNSSSelector.js
@@ -1,14 +1,16 @@
 import './components_css/GNSSSelector.css'
 
-function GNSSSelector() {
+function GNSSSelector(props) {
 
     return (
         <div class="gnssselectorwrapper">
-            <label class="gnssselectorlabel">GNSS Source:</label>
-            <select class="gnssselector">
-              <option value="Drone">Drone</option>
-              <option value="Ground_Station">Ground Station</option>
-            </select>
+            <label class="gnssselectorlabel">GNSS Source:
+                <select class="gnssselector" name={props.name} id={props.name} value={props.value} onChange={(e)=>{props.onChange(e)}}>
+                    <option value="Drone">Drone</option>
+                    <option value="Ground_Station">Ground Station</option>
+                </select>
+            </label>
+
         </div>
     );
 
